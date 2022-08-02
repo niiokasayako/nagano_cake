bc0d421a613c5ef393d8487c5219df26918e132c
niiokasayako <107908957+niiokasayako@users.noreply.github.com>
2022-08-02 09:01:55 +0000

    [Add]genres index

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Rails.application.routes.draw do
-  # 管理者用
-  # URL /admin/sign_in ...
-  devise_for :admin, skip: [registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
-  
-  
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers, skip: [:passwords], controllers: {
     ragistrations: "public/registrations",
-    sessions: 'public/sessions'
+    sessions: "public/sessions"
   }
+  
+  
+   # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  namespace :admin do
+    get '/admin' => 'homes#top'
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, only: [:new, :index, :create, :show, :edit, :update]
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
